a9a0ff1d866d8b8661479019c2eb0dc251363880
Mateus Rocha <mateus046@gmail.com>
2023-09-25 17:00:48 -0300

    blog

--- src/components/CategoryList/index.jsx
+++ src/components/CategoryList/index.jsx
@@ -1,10 +1,68 @@
 import React from "react";
 import styles from './styles.module.css'
+import Link from "next/link";
+import Image from "next/image";
 
 const CategoryList = () => {
     return (
         <div className={styles.container}>
-            categordylist
+            <h1 className={styles.title}>Popular categories</h1>
+            <div className={styles.categories}>
+                    <Link href="/blog?cat=style" className={`${styles.category} ${styles.style}`}>
+                        <Image 
+                            src="/style.png" 
+                            alt="" 
+                            width={32} 
+                            height={32}
+                            className={styles.image} />
+
+                    </Link>
+                    <Link href="" className={`${styles.category} ${styles.fashion}`}>
+                        <Image
+                            src="/fashion.png" 
+                            alt=""
+                            width={32}
+                            height={32}
+                            className={styles.image}
+                        />
+                    </Link>
+                    <Link href="" className={`${styles.category} ${styles.food}`}>
+                        <Image
+                            src="/food.png"
+                            alt=""
+                            width={32}
+                            height={32}
+                            className={styles.image}
+                        />
+                    </Link>
+                    <Link href="" className={`${styles.category} ${styles.coding}`}>
+                        <Image
+                            src="/coding.png"
+                            alt=""
+                            width={32}
+                            height={32}
+                            className={styles.image}
+                        />
+                    </Link>
+                    <Link href="" className={`${styles.category} ${styles.culture}`}>
+                        <Image
+                            src="/culture.png"
+                            alt=""
+                            width={32}
+                            height={32}
+                            className={styles.image}
+                        />
+                    </Link>
+                    <Link href="" className={`${styles.category} ${styles.travel}`}>
+                        <Image
+                            src="/travel.png"
+                            alt=""
+                            width={32}
+                            height={32}
+                            className={styles.image}
+                        />
+                    </Link>
+            </div>
         </div>
     )
 }
