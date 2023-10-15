40e26bc304213576b0f8e5a75f00e71ce0a73daf
mateusrr <mateus046@gmail.com>
2023-10-15 11:29:23 -0300

    updates

--- src/components/CardList/index.jsx
+++ src/components/CardList/index.jsx
@@ -1,8 +1,7 @@
-"use client"
-import React, { useEffect, useState } from "react";
+import React from "react";
 import styles from "./styles.module.css";
 import Pagination from "../Pagination/index";
-import Image from "next/image";
+// import Image from "next/image";
 import Card from "../Card/index";
 
 const getData = async (page, cat) => {
@@ -20,54 +19,21 @@ const getData = async (page, cat) => {
   return res.json();
 };
 
-const CardList = ({ page, cat }) => {
-  const [allPosts, setAllPosts] = useState([]);
+const CardList = async ({ page, cat }) => {
+  const { posts, count } = await getData(page, cat);
+
+  const reversedPosts = posts.slice().reverse()
+
   const POST_PER_PAGE = 2;
 
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        // Busque os posts de todas as páginas disponíveis
-        const allPostsData = [];
-        let currentPage = 1;
-
-        while (true) {
-          const { posts, count } = await getData(currentPage, cat);
-          if (posts.length === 0) {
-            break; // Não há mais posts disponíveis
-          }
-          allPostsData.push(...posts);
-          currentPage++;
-        }
-
-        // Ordene todos os posts por data de criação em ordem decrescente
-        const sortedPosts = allPostsData.sort((a, b) => {
-          const dateA = new Date(a.createdAt).getTime();
-          const dateB = new Date(b.createdAt).getTime();
-          return dateB - dateA;
-        });
-
-        setAllPosts(sortedPosts);
-      } catch (error) {
-        console.error("Erro ao buscar os posts", error);
-      }
-    }
-    fetchData();
-  }, [cat]);
-
   const hasPrev = POST_PER_PAGE * (page - 1) > 0;
-  const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < allPosts.length;
-
-  // Calcule a faixa de posts a serem exibidos na página atual
-  const start = POST_PER_PAGE * (page - 1);
-  const end = start + POST_PER_PAGE;
-  const postsToDisplay = allPosts.slice(start, end);
+  const hasNext = POST_PER_PAGE * (page - 1) + POST_PER_PAGE < count;
 
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>Recent Posts</h1>
-      <div>
-        {postsToDisplay?.map((item) => (
+      <div className={styles.posts}>
+        {reversedPosts?.map((item) => (
           <Card item={item} key={item._id} />
         ))}
       </div>
