c6942d187cb1c2aaa1644d0cc06e3ed3265f68a7
mateusrr <mateus046@gmail.com>
2023-10-17 21:22:00 -0300

    updates

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -3,30 +3,27 @@ import Image from "next/image";
 import Comments from "@/components/Comments/index";
 
 const getData = async (slug) => {
-  if(typeof document !== "undefined") {
-    const res = await fetch(`https://the-blog-lake.vercel.app/api/posts/${slug}?popular=true`, {
-      cache: "no-store",
-  })
+  const res = await fetch(`https://the-blog-lake.vercel.app/api/posts/${slug}`, {
+    cache: "no-store",
+  });
 
-  if(!res.ok) {
-      throw new Error("failed")
+  if (!res.ok) {
+    throw new Error("Failed");
   }
 
-  }
-  return res.json()
-}
+  return res.json();
+};
 
-const SinglePage = async ({params}) => {
-  const {slug} = params
-  const data = await getData(slug)
+const SinglePage = async ({ params }) => {
+  const { slug } = params;
+
+  const data = await getData(slug);
 
   return (
     <div className={styles.container}>
       <div className={styles.infoContainer}>
         <div className={styles.textContainer}>
-          <h1 className={styles.title}>
-            {data?.title}
-          </h1>
+          <h1 className={styles.title}>{data?.title}</h1>
           <div className={styles.user}>
             {data?.user?.image && (
               <div className={styles.userImageContainer}>
@@ -35,13 +32,13 @@ const SinglePage = async ({params}) => {
             )}
             <div className={styles.userTextContainer}>
               <span className={styles.username}>{data?.user.name}</span>
-              <span className={styles.date}>{data?.createdAt}</span>
+              <span className={styles.date}>01.01.2024</span>
             </div>
           </div>
         </div>
         {data?.img && (
           <div className={styles.imageContainer}>
-             <Image src={data.img} alt="" fill className={styles.image} />
+            <Image src={data.img} alt="" fill className={styles.image} />
           </div>
         )}
       </div>
@@ -49,12 +46,10 @@ const SinglePage = async ({params}) => {
         <div className={styles.post}>
           <div
             className={styles.description}
-            // dangerouslySetInnerHTML={{__html:data?.asc || ''}}
-          >
-          <p>{data.desc}</p>
-          </div>
+            dangerouslySetInnerHTML={{ __html: data?.desc }}
+          />
           <div className={styles.comment}>
-            <Comments postSlug={slug} />
+            <Comments postSlug={slug}/>
           </div>
         </div>
       </div>
