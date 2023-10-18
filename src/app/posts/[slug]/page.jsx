87a3216c04e289ca0acda658141e589b736f1821
mateusrr <mateus046@gmail.com>
2023-10-17 21:49:34 -0300

    updates

--- src/app/posts/[slug]/page.jsx
+++ src/app/posts/[slug]/page.jsx
@@ -32,7 +32,9 @@ const SinglePage = async ({ params }) => {
             )}
             <div className={styles.userTextContainer}>
               <span className={styles.username}>{data?.user.name}</span>
-              <span className={styles.date}>{data?.createdAt}</span>
+              <span className={styles.date}>
+                {data ? format(new Date(data.createdAt), 'dd/MM/yyyy HH:mm:ss') : ''}
+              </span>
             </div>
           </div>
         </div>
