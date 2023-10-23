8f05293307720c2470aec3a8e73180d3765f2171
mateusrr <mateus046@gmail.com>
2023-10-23 10:36:17 -0300

    updates

--- src/components/Featured/index.jsx
+++ src/components/Featured/index.jsx
@@ -7,20 +7,22 @@ const Featured = async () => {
     <div className={styles.container}>
       <h1 className={styles.title}>
       {/* Vamos explorar o mundo. <br/> */}
-      Compartilhe suas experiências!
+      Compartilhe suas memórias!
       </h1>
       <div className={styles.post}>
+        <div className={styles.textContainer}>
+          <h1 className={styles.postTitle}>Conecte-se com histórias
+          inspiradoras <br/>e emocionantes em nossa comunidade.</h1>
+          {/* <p className={styles.postDesc}>
+          Que cada história, seja inspiradora, engraçada, emocionante
+          e valorizada, criando uma comunidade de conexão e inspiração
+          <br/>por meio das experiências de vida. <br/>Junte-se a nós para eternizar momentos
+          especiais e conectar-se <br/>com outras pessoas que compartilham vivências similares.
+          </p> */}
+          <p>Faça o login e comece a publicar.</p>
+        </div>
         <div className={styles.imgContainer}>
           <Image src="/booking.jpg" alt="" fill className={styles.image} />
-        </div>
-        <div className={styles.textContainer}>
-          <h1 className={styles.postTitle}>A curiosidade é o combustível
-          para o crescimento pessoal.</h1>
-          <p className={styles.postDesc}>
-          Se você compartilha nossa
-          paixão pela exploração intelectual e está <br/>em busca de inspiração, conhecimento
-          e diversão, este blog é o seu <br/>destino.
-          </p>
         </div>
       </div>
     </div>
