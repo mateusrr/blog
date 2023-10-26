291d21f879760e1527452764ce46165b3216eed6
mateusrr <mateus046@gmail.com>
2023-10-26 13:32:14 -0300

    updates

--- src/components/Featured/index.jsx
+++ src/components/Featured/index.jsx
@@ -13,18 +13,19 @@ const Featured = () => {
     <div className={styles.container}>
       <h1 className={styles.title}>
       {/* Vamos explorar o mundo. <br/> */}
-      Compartilhe suas memórias!
+      {/* Compartilhe suas memórias! */}
+      Conecte-se com histórias inspiradoras
       </h1>
       <div className={styles.post}>
         <div className={styles.textContainer}>
-          <h1 className={styles.postTitle}>Conecte-se com histórias
-          inspiradoras <br/>e emocionantes em nossa comunidade.</h1>
-          {/* <p className={styles.postDesc}>
-          Que cada história, seja inspiradora, engraçada, emocionante
+          <h1 className={styles.postTitle}>Compartilhe suas memórias!</h1>
+          <p className={styles.postDesc}>
+          {/* Que cada história, seja inspiradora, engraçada, emocionante
           e valorizada, criando uma comunidade de conexão e inspiração
-          <br/>por meio das experiências de vida. <br/>Junte-se a nós para eternizar momentos
-          especiais e conectar-se <br/>com outras pessoas que compartilham vivências similares.
-          </p> */}
+          <br/>por meio das experiências de vida. */}
+          Junte-se a nós para eternizar momentos
+          especiais e conectar-se com outras pessoas que compartilham vivências similares.
+          </p>
           <div className={styles.log}>
           <>
             {status === "unauthenticated" ? (
