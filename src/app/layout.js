d0ecfc680988667b1b278cef57e31c9349e31889
mateusrr <mateus046@gmail.com>
2024-06-15 11:54:39 -0300

    updates

--- src/app/layout.js
+++ src/app/layout.js
@@ -15,20 +15,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-    <head>
-      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5562188841194106"
-      crossorigin="anonymous"></script>
-      {/* Bloco1 */}
-      <ins class="adsbygoogle"
-        style="display:block"
-        data-ad-client="ca-pub-5562188841194106"
-        data-ad-slot="5786915546"
-        data-ad-format="auto"
-        data-full-width-responsive="true"></ins>
-      <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-      </script>
-    </head>
       <body className={inter.className}>
         <AuthProvider>
           <ThemeContextProvider>
@@ -37,6 +23,18 @@ export default function RootLayout({ children }) {
                 <div className="wrapper">
                   <Navbar />
                   {children}
+                  <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5562188841194106"
+                  crossorigin="anonymous"></script>
+                  {/* Bloco1 */}
+                  <ins class="adsbygoogle"
+                    style="display:block"
+                    data-ad-client="ca-pub-5562188841194106"
+                    data-ad-slot="5786915546"
+                    data-ad-format="auto"
+                    data-full-width-responsive="true"></ins>
+                  <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                  </script>
                 </div>
               </div>
             </ThemeProvider>
