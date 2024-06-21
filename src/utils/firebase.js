551954f7aa61e61c9d4e92662e7cc3dca68ece7b
mateusrr <mateus046@gmail.com>
2024-06-21 14:20:41 -0300

    update

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -2,11 +2,11 @@ import { initializeApp } from "firebase/app";
 
 const firebaseConfig = {
   apiKey: process.env.FIREBASE,
-  authDomain: "blogapp-87604.firebaseapp.com",
-  projectId: "blogapp-87604",
-  storageBucket: "blogapp-87604.appspot.com",
-  messagingSenderId: "120899485763",
-  appId: "1:120899485763:web:f0db2c3bf6877bbfc054f6"
+  authDomain: process.env.AUTH_DOMAIN,
+  projectId: process.env.PROJECT_ID,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID,
+  appId: process.env.APP_ID
 };
 
 export const app = initializeApp(firebaseConfig);
